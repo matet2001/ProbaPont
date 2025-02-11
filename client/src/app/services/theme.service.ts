f5bbbce6353f451ee24810c9021c67b3a38943a2
matet2001 <matet2001@gmail.com>
2025-02-11 13:13:43 +0100

    Solving theme reset on reload

--- client/src/app/services/theme.service.ts
+++ client/src/app/services/theme.service.ts
@@ -1,40 +1,34 @@
-import { Injectable, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { isPlatformBrowser } from '@angular/common';
+import {DOCUMENT} from "@angular/common";
 
 @Injectable({
   providedIn: 'root',
 })
-export class ThemeService implements OnInit {
+export class ThemeService {
   private isDarkModeSubject = new BehaviorSubject<boolean>(false);
   isDarkMode$ = this.isDarkModeSubject.asObservable();
 
-  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    const localStorage = document.defaultView?.localStorage;
 
-  ngOnInit() {
-    this.loadTheme();
-  }
-
-  private loadTheme() {
-    if (isPlatformBrowser(this.platformId)) {
+    if (localStorage) {
       const storedTheme = localStorage.getItem('theme');
       const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
       const isDark = storedTheme ? storedTheme === 'dark' : systemPrefersDark;
-      this.setDarkMode(isDark);
+
+      this.setDarkMode(isDark); // Initialize theme
     }
   }
 
   setDarkMode(darkMode: boolean) {
     this.isDarkModeSubject.next(darkMode);
+    localStorage.setItem('theme', darkMode ? 'dark' : 'light');
 
-    if (isPlatformBrowser(this.platformId)) {
-      localStorage.setItem('theme', darkMode ? 'dark' : 'light');
-
-      if (darkMode) {
-        document.body.classList.add('dark');
-      } else {
-        document.body.classList.remove('dark');
-      }
+    if (darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
     }
   }
 }
