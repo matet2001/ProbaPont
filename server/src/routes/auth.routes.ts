d88d4e432a9266c4a90c536f9c1103801209f7e2
mmarci96 <sarosdimarci@gmail.com>
2025-02-20 20:23:28 +0100

    indent

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,23 +1,25 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { createUser } from '../services/user.service';
-import { createToken } from '../services/auth.service';
+import express, { NextFunction, Request, Response } from "express";
+import { createUser } from "../services/user.service";
+import { createToken } from "../services/auth.service";
 
 const router = express.Router();
 
-router.post('/signup',
+router.post(
+    "/signup",
     async (req: Request, res: Response, next: NextFunction): Promise<any> => {
         try {
             const { username, email, password } = req.body;
             const user = await createUser(username, email, password);
+
             res.status(201).send({ data: user });
         } catch (err) {
-            next(err)
+            next(err);
         }
-    }
-)
+    },
+);
 
 router.post(
-    '/login',
+    "/login",
     async (req: Request, res: Response, next: NextFunction): Promise<any> => {
         try {
             const { email, password } = req.body;
@@ -29,6 +31,5 @@ router.post(
         }
     },
 );
-
 
 export default router;
