df72945a776186ede8902e709052bb95c9580fa7
matet2001 <matet2001@gmail.com>
2025-03-18 10:08:23 +0100

    Switch to atlas

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,50 @@
-import mongoose from "mongoose";
-import app from "./app";
+// import mongoose from "mongoose";
+// import app from "./app";
+// import { config } from "./config";
+//
+//
+// const main = async () => {
+//     try {
+//         await mongoose.connect(MONGO_URI);
+//         console.log("Connected to mongo db");
+//
+//         app.listen(Number(PORT), HOST, () => {
+//             console.log(`Server is running on http://${HOST}:${PORT}`);
+//         });
+//     } catch (err) {
+//         console.error(err);
+//     }
+// };
+
+// main();
 import { config } from "./config";
 
-const { MONGO_URI, PORT, HOST } = config;
+const { DB_PASSWORD } = config;
 
-const main = async () => {
-    try {
-        await mongoose.connect(MONGO_URI);
-        console.log("Connected to mongo db");
+import {MongoClient, ServerApiVersion} from 'mongodb';
+const uri = `mongodb+srv://matet2001:${DB_PASSWORD}@learncluster.txclaxs.mongodb.net/?retryWrites=true&w=majority&appName=LearnCluster`;
 
-        app.listen(Number(PORT), HOST, () => {
-            console.log(`Server is running on http://${HOST}:${PORT}`);
-        });
-    } catch (err) {
-        console.error(err);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
     }
-};
+});
 
-main();
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Server listening on 8080!");
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
